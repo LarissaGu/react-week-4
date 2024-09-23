8ae8bd5f229ff1c91cd37518ad796c4616a29452
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2024-09-23 08:50:28 +0200

    Added link to GitHub Repository

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <div>
+        <a
+          href="https://github.com/LarissaGu/react-week-4"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub Repository
+        </a>{" "}
+        for this project
+      </div>
     </div>
   );
 }
